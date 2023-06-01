4c9dbade7e67306c7ffdc08a8c09726fa1f5968b
uv.turov <uv.turov@tensor.ru>
2023-06-01 15:11:38 +0300

    Задачи(решения-исправления2) с 8го вебинара

--- Homework8/task_1.py
+++ Homework8/task_1.py
@@ -22,14 +22,14 @@ def treatment_sum(our_tuple):
     """
     Функция обрабатывает исключения
     """
-    if len(our_tuple) > 2:
-        raise Exception('Много данных')
     try:
         res = our_tuple[0] + our_tuple[1]
     except IndexError:
         return 'Недостаточно данных'
     except TypeError:
         return 'Нельзя сложить эти данные'
+    if len(our_tuple) > 2:
+        raise Exception('Много данных')
     return res
 
 
