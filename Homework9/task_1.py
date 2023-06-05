65406ad2d7c7457221e1e40cc3fe21a7c7560ed3
uv.turov <uv.turov@tensor.ru>
2023-06-05 15:19:13 +0300

    Задачи с 9го вебинара

---
+++ Homework9/task_1.py
@@ -0,0 +1,16 @@
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
+
+
+# Здесь пишем код
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
