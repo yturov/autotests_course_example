8b20bf287c4b12ec0d771f4fec1886930a7f02f1
uv.turov <uv.turov@tensor.ru>
2023-06-07 15:39:44 +0300

    Задачи(решения) с 10го вебинара

--- Homework10/task_2.py
+++ Homework10/task_2.py
@@ -10,8 +10,55 @@ import pytest
 
 
 def all_division(*arg1):
-
     division = arg1[0]
     for i in arg1[1:]:
         division /= i
     return division
+
+
+def test_positive_01():
+    """
+    Проверка деления ноля на число
+    """
+    result = all_division(0, 1, 1)
+    assert result == 0, f"при делении 0 на число в результате ожидаем ноль \n" \
+                        f"входящие данные (0, 1, 1) \n" \
+                        f"функция вернула {result}"
+
+
+@pytest.mark.smoke
+def test_positive_02():
+    """
+    Проверка деления натуральных чисел
+    """
+    result = all_division(10, 2, 5)
+    assert result == 1, f"при делении натуральных чисел в результате ожидаем 1\n" \
+                        f"входящие данные (10, 2, 5)  \n" \
+                        f"функция вернула {result}"
+
+
+def test_positive_03():
+    """
+    Проверка деления дробных чисел
+    """
+    result = all_division(10.2, 0.5)
+    assert result == 20.4, f"при делении дробных чисел в результате ожидаем 20.4\n" \
+                           f"входящие данные (10.2, 0.5)  \n" \
+                           f"функция вернула {result}"
+
+
+def test_negative_01():
+    """
+    Передача в функцию данных отличных от числа
+    """
+    with pytest.raises(TypeError):
+        all_division("10", 2)
+
+
+@pytest.mark.smoke
+def test_negative_02():
+    """
+    Проверка исключения при делении на ноль
+    """
+    with pytest.raises(ZeroDivisionError):
+        all_division(10, 0)
--- Homework10/task_3.py
+++ Homework10/task_3.py
@@ -12,5 +12,32 @@ def all_division(*arg1):
     return division
 
 
+@pytest.mark.parametrize("set_value,result",
+                         [
+                             pytest.param((10, 2, 5), 1, marks=pytest.mark.skip("плохие данные")),
+                             pytest.param((0, 1, 1), 0, marks=pytest.mark.smoke),
+                             ((10.2, 0.5), 20.4),
+                         ])
+def test_positive(set_value, result):
+    """
+    Проверка all_division через parametrize позитивные сценарии
+    """
+    result_res = all_division(*set_value)
+    assert result == result_res, f"при делении натуральных чисел в результате ожидаем 1\n" \
+                                 f"входящие данные {set_value}  \n" \
+                                 f"функция вернула {result}"
 
+
+@pytest.mark.parametrize("set_value,type_error",
+                         [
+                             pytest.param(("10", "2", "5"), TypeError, marks=pytest.mark.skip("плохие данные")),
+                             pytest.param(("10", 1,), TypeError, marks=pytest.mark.smoke),
+                             ((10, 0), ZeroDivisionError),
+                         ], ids=["lot bad data", "TypeError", "ZeroDivisionError"])
+def test_negative(set_value, type_error):
+    """
+    Проверка all_division через parametrize негативные сценарии
+    """
+    with pytest.raises(type_error):
+        all_division(*set_value)
 
