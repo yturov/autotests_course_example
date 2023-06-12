6d972690d545b4f18c09b5d0f3a88cec2ead9f5b
uv.turov <uv.turov@tensor.ru>
2023-06-12 22:40:43 +0300

    Задачи с 11го вебинара

---
+++ homework11/task_2.py
@@ -0,0 +1,10 @@
+# Авторизоваться на сайте https://fix-online.sbis.ru/
+# Перейти в реестр Контакты
+# Отправить сообщение самому себе
+# Убедиться, что сообщение появилось в реестре
+# Удалить это сообщение и убедиться, что удалили
+# Для сдачи задания пришлите код и запись с экрана прохождения теста
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
